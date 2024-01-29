ee347f0f8151f285128a1a94379065ad29889a67
Balaji-T001 <balajikalai_cs@mepcoeng.ac.in>
2024-01-29 23:57:52 +0530

    Submission 7

--- src/main/java/com/driver/services/WebSeriesService.java
+++ src/main/java/com/driver/services/WebSeriesService.java
@@ -42,9 +42,9 @@ public class WebSeriesService {
         productionHouse.setRatings(newRating);
         webSeries.setProductionHouse(productionHouse);
         productionHouse.getWebSeriesList().add(webSeries);
-//        WebSeries newWebSeriesAdded = webSeriesRepository.save(webSeries);
-        productionHouseRepository.save(productionHouse);
-        return webSeries.getId();
+        WebSeries newWebSeriesAdded = webSeriesRepository.save(webSeries);
+//        productionHouseRepository.save(productionHouse);
+        return newWebSeriesAdded.getId();
     }
 
 }
