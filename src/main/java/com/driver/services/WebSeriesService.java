df6d4f73f99d5ad93071c9fa9b251a7ebb7b2e05
Balaji-T001 <balajikalai_cs@mepcoeng.ac.in>
2024-01-30 02:08:59 +0530

    Submission 8

--- src/main/java/com/driver/services/WebSeriesService.java
+++ src/main/java/com/driver/services/WebSeriesService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WebSeriesService {
@@ -19,20 +20,26 @@ public class WebSeriesService {
     @Autowired
     ProductionHouseRepository productionHouseRepository;
 
+    public WebSeries findBySeriesName(String seriesName){
+        WebSeries webSeries = webSeriesRepository.findBySeriesName(seriesName);
+        return webSeries;
+    }
+
     public Integer addWebSeries(WebSeriesEntryDto webSeriesEntryDto)throws  Exception{
 
         //Add a webSeries to the database and update the ratings of the productionHouse
         //Incase the seriesName is already present in the Db throw Exception("Series is already present")
         //use function written in Repository Layer for the same
         //Dont forget to save the production and webseries Repo
+        WebSeries webSeries1 = findBySeriesName(webSeriesEntryDto.getSeriesName());
+        if(webSeries1!=null){
+            throw new Exception("Series is already present");
+        }
         ProductionHouse productionHouse = productionHouseRepository.findById(webSeriesEntryDto.getProductionHouseId()).get();
         List<WebSeries> webSeriesList = productionHouse.getWebSeriesList();
         double ratingSum=0;
         int webSeriesCount=0;
         for(WebSeries webSeries:webSeriesList){
-            if(webSeries.getSeriesName().equals(webSeriesEntryDto.getSeriesName())==true){
-                throw new Exception("Series is already present");
-            }
             ratingSum=ratingSum+webSeries.getRating();
             webSeriesCount+=1;
         }
